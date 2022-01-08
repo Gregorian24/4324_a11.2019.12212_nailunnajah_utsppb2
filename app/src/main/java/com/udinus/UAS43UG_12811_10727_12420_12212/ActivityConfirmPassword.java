be5cbe658a0ba5c011ceb6bd4bbc334b6dff44e9
Gregorian <gregorianmargono@gmail.com>
2022-01-08 11:10:36 +0700

    Major Changes to Activity Files!

---
+++ app/src/main/java/com/udinus/UAS43UG_12811_10727_12420_12212/ActivityConfirmPassword.java
@@ -0,0 +1,14 @@
+package com.udinus.UAS43UG_12811_10727_12420_12212;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityConfirmPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_password);
+    }
+}
---
+++ app/src/main/java/com/udinus/UAS43UG_12811_10727_12420_12212/ActivitySelectionCar.java
@@ -0,0 +1,14 @@
+package com.udinus.UAS43UG_12811_10727_12420_12212;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySelectionCar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_car);
+    }
+}
